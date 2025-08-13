77bc25345f07bb435270a5b7ad9ecf1ab933e2cd
SahilHedaooRadical <sahil.h@radicalglobal.net>
2025-08-13 17:02:57 +0530

    webhosted

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const userRoutes = require('./routes/userRoutes');
 const collectionRoutes = require('./routes/collectionRoutes');
 const returnRoutes = require('./routes/returnRoutes');
 
-const PORT = process.env.PORT || 3002;
+
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://user-web-9wez.vercel.app" // your hosted frontend
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/banks', bankRoutes);
@@ -19,6 +26,6 @@ app.get('/', (req, res) => {
   res.send("hello bank");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, '0.0.0.0', () => {
+  console.log('Server running on port 5000');
 });
