f15289fae7018af7d229d61c6dd5faa2a52d7b35
Prathamesh Nimje <prathmesh.n@radicalglobal.net>
2026-05-27 10:40:47 +0530

    bug fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const returnRoutes = require('./routes/returnRoutes');
 
 dotenv.config();
 
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || '3002';
 const app = express();
 app.use(cors());
 app.use(express.json());
